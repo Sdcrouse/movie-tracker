651efd6b4365df4dd9d6c648ebd83c8cfd2c50df
Sdcrouse <sdcrouse91@gmail.com>
2025-05-08 01:13:55 -0700

    Added a registration form, validations, flash messages, and test cases

--- movie_tracker/auth.py
+++ movie_tracker/auth.py
@@ -1,9 +1,28 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, request, flash, render_template
 )
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/register')
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        # TODO: Ensure that usernames and passwords are valid
+        # TODO: Ensure that the username doesn't already exist
+
+        if error is None:
+            # TODO: Change this to redirect to the login page
+            flash('Registration is successful!')
+
+        flash(error)
+    
     return render_template('auth/register.html')
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,25 @@
+import pytest
+
 def test_register(client):
     response = client.get('/auth/register')
 
     assert response.status_code == 200
     assert b'Register here!' in response.data
+
+    # TODO: Ensure that the user is redirected to the login page after registering
+
+@pytest.mark.parametrize(
+    ('username', 'password', 'message'),
+    (
+        ('', 'paswrd', b'Username is required.'),
+        ('userA', '', b'Password is required.'),
+        # TODO: Add test cases for invalid usernames and passwords
+        # TODO: Add a test case for a user who is already registered
+    )
+)
+def test_register_with_invalid_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
